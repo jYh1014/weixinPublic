de1b312e2b07e224a9789b8c1dd719d2beb11284
jiangying <3200371428@qq.com>
2017-11-22 14:12:43 +0800

    几种消息的回复

--- server/wechat/reply.js
+++ server/wechat/reply.js
@@ -2,6 +2,34 @@ const tip = "我是皮皮猪 "
 export default async (ctx,next) => {
    
     const message = ctx.weixin
-    // console.log(message)
-    ctx.body = tip
+    console.log(message)
+    if(message.MsgType === 'text'){
+        ctx.body = message.Content
+    }else if(message.MsgType === 'image'){
+        ctx.body = {
+            type: 'image',
+            mediaId: message.MediaId
+        }
+    }else if(message.MsgType === 'voice'){
+        ctx.body = {
+            type: 'voice',
+            mediaId: message.MediaId
+        }
+    }else if(message.MsgType === 'video'){
+        ctx.body = {
+            type: 'video',
+            title: message.ThumbMediaId,
+            mediaId: message.MediaId
+        }
+    }else if(message.MsgType === 'location'){
+        ctx.body = message.Location_X + ':' + message.Location_Y + message.Label
+    }else if(message.MsgType === 'link'){
+        ctx.body = [{
+            title:message.Title,
+            description:message.Description,
+            picUrl:'http://mmbiz.qpic.cn/mmbiz_jpg/JLyiaIHgfCwrxO7SpIhVaXVu9jd6yIOvelEmsBuyBYmVcWM4pt7FaVhfnkP2TdKl7B44hF9h07ZkdFibu9ibv0xnw/0',
+            url:message.Url
+        }]
+    }
+    
 }
