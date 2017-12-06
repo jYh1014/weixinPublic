bb10a304c38a67db81fb94158f5665a87592918c
jiangying <3200371428@qq.com>
2017-12-06 16:03:26 +0800

    增加用户模型scheme

---
+++ pages/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="container">
+    <img src="../static/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    
+  </section>
+</template>
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
